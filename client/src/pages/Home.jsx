bc4ba05e7a2c96128c0a1adfb6c2c73393d7d74b
Yogesh Kumar <yogeshkumar@ta960.wpa.dal.ca>
2023-03-07 11:30:52 -0400

    home page cards created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Card, FormField } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  const RenderCards = ({ data, title }) => {
+    if (data?.length > 0) {
+      data.map((post) => {
+        <Card key={post._id} {...post} />;
+      });
+    }
+    return <h4> {title} </h4>;
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>Imaginative images</h3>
+        <h4>Showing you the art gallery by our artists</h4>
+      </div>
+
+      <div>
+        <RenderCards data={[]} title="No posts found" />
+      </div>
+
+      <div className="grid lg:grid-cols-4 grid-cols-1 gap-3"></div>
+    </div> //main div
+  );
 };
 
 export default Home;
